341e74105d3c0aba6fa782407ce2d963632782bc
Parjanya-Kumar-Arya-789 <85034168+Parjanya-Kumar-Arya-789@users.noreply.github.com>
2021-05-30 10:19:26 +0530

    Add files via upload
    This is as Ascii-simulation generator which will generate an Ascii simulation of the video here it is "blackdance.mp4" with background music "Song.mp3".

---
+++ ascii_generator.py
@@ -0,0 +1,53 @@
+import time
+
+from PIL import Image
+import cv2  # pip install opencv2
+import os
+from pygame import mixer  # pip install pygame
+import sys
+import threading
+
+mixer.init()  # Initialzing pyamge mixer
+
+mixer.music.load('Song.mp3')  # Add your own song here
+if not (os.path.isdir('Frame')):
+    vidObj = cv2.VideoCapture("blackdance.mp4")  # Add your own video here.
+    count = 0
+    success = 1
+
+    os.makedirs('Frame')
+    while success:
+        success, image = vidObj.read()
+        cv2.imwrite("Frame/frame%d.jpg" % count, image)
+        count += 1
+musicplay = threading.Thread(target=mixer.music.play())
+
+
+def player():
+    run = True
+    i = 0
+    while run:
+        try:
+            img = Image.open(f"Frame/frame{i}.jpg")
+            img = img.convert('L')
+            ascii_string = " .:-=$*#%@"
+            img = img.resize((100, 50))
+            pixels = list(img.getdata())
+            w, h = img.size
+            Ascii_art = ""
+            for pixel in range(len(pixels)):
+                if pixel % w == 0:
+                    Ascii_art = Ascii_art + "\n"
+                Ascii_art = Ascii_art + ascii_string[pixels[pixel] // 50]
+
+            print(Ascii_art)
+
+            i += 1
+            time.sleep(0.05)
+            os.system('cls')
+        except Exception as e:
+            run = False
+            sys.exit()
+
+
+player()
